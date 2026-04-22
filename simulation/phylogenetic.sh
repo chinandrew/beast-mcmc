6d32edeab7fee8024151b1aafd2930548d123067
Andrew Chin <chinandrew96@gmail.com>
2026-04-22 11:04:58 -0400

    Update run parameters

--- simulation/phylogenetic.sh
+++ simulation/phylogenetic.sh
@@ -11,3 +11,7 @@ mkdir "${output}_${SLURM_ARRAY_TASK_ID}"
 cd "${output}_${SLURM_ARRAY_TASK_ID}"
 time java -jar /users/achin/beast-mcmc/build/dist/beast.jar -seed $SLURM_ARRAY_TASK_ID -overwrite /users/achin/beast-mcmc/simulation/xml/$xml
 
+# sbatch --array=666-670 --nodelist=compute-124 --export=output=hbps_0-5_5M,xml=24t_HBPS_0-5.xml --job-name=hiv0-5 --mem=15G phylogenetic.sh
+# sbatch --array=666-670 --nodelist=compute-124 --export=output=hbps_1_2-5M,xml=24t_HBPS_1.xml --job-name=hiv1 --mem=15G phylogenetic.sh
+# sbatch --array=666-670 --nodelist=compute-124 --export=output=bps_0-5_5M,xml=24t_BPS.xml --job-name=hivbps --mem=15G phylogenetic.sh
+# sbatch --array=666-670 --nodelist=compute-124 --export=output=split_50K,xml=24t_HNUTS_split.xml --job-name=hivsplit --mem=15G phylogenetic.sh
