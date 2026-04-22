8ba22dba0c08783a611c9af885c55e7f9d56fe92
Andrew Chin <andrew.chin.ae@gmail.com>
2026-04-22 10:54:55 -0400

    update run script

--- simulation/phylogenetic.sh
+++ simulation/phylogenetic.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+#SBATCH --mail-user=achin23@jhu.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --time=720:00:00
+
 module load conda
 source activate env
 source activate env
-cd hbps/hiv_sim2/
-mkdir $output
-cd $output
-time java -jar /users/achin/beast-mcmc/build/dist/beast.jar -seed $seed -overwrite /users/achin/beast-mcmc/simulation/xml/$xml
+cd /fastscratch/myscratch/achin/hiv_sim
+mkdir "${output}_${SLURM_ARRAY_TASK_ID}"
+cd "${output}_${SLURM_ARRAY_TASK_ID}"
+time java -jar /users/achin/beast-mcmc/build/dist/beast.jar -seed $SLURM_ARRAY_TASK_ID -overwrite /users/achin/beast-mcmc/simulation/xml/$xml
 
